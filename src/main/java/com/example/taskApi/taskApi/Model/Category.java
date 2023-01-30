e4225eecbdc4857bceeb67e7ea9e83dfbb544390
Maria Carolina Xavier <mcs_nasc@hotmail.com>
2023-01-30 14:36:00 -0300

    Adiciono a classe Category

--- src/main/java/com/example/taskApi/taskApi/Model/Category.java
+++ src/main/java/com/example/taskApi/taskApi/Model/Category.java
@@ -11,5 +11,63 @@ import java.util.UUID;
 @Table
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
 
+    @NonNull
+    private String type;
+
+    @NonNull
+    private String description;
+
+    @NonNull
+    private String keyword;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties({"category", "task"})
+    private List <Task> task;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getType() {
+        return type;
+    }
+
+    public void setType(@NonNull String type) {
+        this.type = type;
+    }
+
+    @NonNull
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@NonNull String description) {
+        this.description = description;
+    }
+
+    @NonNull
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(@NonNull String keyword) {
+        this.keyword = keyword;
+    }
+
+    public List<Task> getTask() {
+        return task;
+    }
+
+    public void setTask(List<Task> task) {
+        this.task = task;
+    }
 }
