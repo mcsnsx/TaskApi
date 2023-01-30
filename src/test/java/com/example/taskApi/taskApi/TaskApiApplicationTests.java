dda5594a5bc48cd3f9a08684cf706ed6ec53ac3a
Maria Carolina Xavier <mcs_nasc@hotmail.com>
2023-01-30 13:57:42 -0300

    Commit Inicial

---
+++ src/test/java/com/example/taskApi/taskApi/TaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskApi.taskApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
